b19de8fba8204d863dd19f6d16786578c8f2a702
DaveMain <swdev.dave@gmail.com>
2018-10-02 17:46:19 -0700

    Completed Practice

---
+++ app/src/main/java/com/swdave/retrofit2_practice/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package com.swdave.retrofit2_practice;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
